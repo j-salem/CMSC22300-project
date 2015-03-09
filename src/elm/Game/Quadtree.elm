5a2c709f9405af3440127a195752f0a62b556f48
j-salem <john.d.salem@gmail.com>
2015-03-09 00:14:15 -0500

    compiled quadtrees (needs tests)

--- src/elm/Game/Quadtree.elm
+++ src/elm/Game/Quadtree.elm
@@ -1,6 +1,7 @@
 module Game.Quadtree where
 
 import Game.Collision (..)
+import Game.Util (fromJust)
 import Trampoline (..)
 import Array as A
 
@@ -8,8 +9,10 @@ import Array as A
 
 -- 2point
 type alias XY = { x:Float, y:Float }
--- Axis Aligned Bounding Box with half dimensions
-type alias AABB = { center:XY, dim:Rect }
+-- Axis Aligned Bounding Box with half dimensions (c -> center, d->dimensions)
+type alias AABB = { center:XY, dimensions:Rect }
+
+type alias Positioned a = { a | x:Float, y:Float }
 
 -- Quadtree containing type a is either:
 -- > Leaf with bounding box, max number of elements, and array containing elems
@@ -17,12 +20,11 @@ type alias AABB = { center:XY, dim:Rect }
 --
 -- Notes: Traverse quadrants in clockwise order starting in NW
 
--- NEED ELM EXTENSIBLE RECORDS
 type Quadtree a =
     Leaf AABB Int (A.Array a) |
     Node AABB (Quadtree a) (Quadtree a) (Quadtree a) (Quadtree a)
 
-emptyQT : AABB -> Int -> Quadtree
+emptyQT : AABB -> Int -> Quadtree a
 emptyQT aabb maxElem =
     Leaf aabb maxElem A.empty
 
@@ -34,23 +36,23 @@ aabbQT qt = case qt of
 maxElemQT : Quadtree a -> Int
 maxElemQT qt = case qt of
     Leaf _ me _       -> me
-    Node _ me _ _ _ _ -> me
+    Node _ nw _ _ _    -> maxElemQT nw
 
 subdivideAABB : AABB -> (AABB, AABB, AABB, AABB)
-subdivideAABB {{x,y},{w,h}} =
+subdivideAABB {center,dimensions} =
     let
-        nwXY = {x = x - w / 2, y = y + h / 2}
-        neXY = {x = x + w / 2, y = y + h / 2}
-        seXY = {x = x + w / 2, y = y - h / 2}
-        swXY = {x = x - w / 2, y = y - h / 2}
-        newRect = {w = w / 2, h = h / 2}
+        nwXY = {x = center.x - dimensions.width / 2, y = center.y + dimensions.height / 2}
+        neXY = {x = center.x + dimensions.width / 2, y = center.y + dimensions.height / 2}
+        seXY = {x = center.x + dimensions.width / 2, y = center.y - dimensions.height / 2}
+        swXY = {x = center.x - dimensions.width / 2, y = center.y - dimensions.height / 2}
+        newRect = {width = dimensions.width / 2, height = dimensions.height / 2}
     in
-    ({center = nwXY, dim = newRect},
-     {center = neXY, dim = newRect},
-     {center = seXY, dim = newRect},
-     {center = swXY, dim = newRect})
+    ({center = nwXY, dimensions = newRect},
+     {center = neXY, dimensions = newRect},
+     {center = seXY, dimensions = newRect},
+     {center = swXY, dimensions = newRect})
 
-subdivide : Quadtree a -> Quadtree a
+subdivide : Quadtree (Positioned a) -> Quadtree (Positioned a)
 subdivide (Leaf aabb maxElem ar) =
     let (nw,ne,se,sw) = subdivideAABB aabb in
     insertMany ar
@@ -61,19 +63,14 @@ subdivide (Leaf aabb maxElem ar) =
             (emptyQT sw maxElem))
 
 -- PRE: type a is required to have an x and y
-insert : a -> Quadtree a -> Quadtree a
+insert : Positioned a -> Quadtree (Positioned a) -> Quadtree (Positioned a)
 insert elem qt = case qt of
     Node aabb nw ne se sw ->
-        if | containsPointQT nw {elem.x,elem.y} -> Node aabb (insert elem nw) ne se sw
-           | containsPointQT ne {elem.x,elem.y} -> Node aabb nw (insert elem ne) se sw
-           | containsPointQT se {elem.x,elem.y} -> Node aabb nw ne (insert elem se) sw
-           | otherwise                          -> Node aabb nw ne se (insert elem sw)
+        if | containsPointQT nw {x=elem.x,y=elem.y} -> Node aabb (insert elem nw) ne se sw
+           | containsPointQT ne {x=elem.x,y=elem.y} -> Node aabb nw (insert elem ne) se sw
+           | containsPointQT se {x=elem.x,y=elem.y} -> Node aabb nw ne (insert elem se) sw
+           | otherwise                              -> Node aabb nw ne se (insert elem sw)
     Leaf aabb maxElem ar ->
-<<<<<<< HEAD
-        if | {- Check if in Array -}     -> Leaf aabb maxElem ar
-           | (A.length ar) + 1 > maxElem -> subdivide (Leaf aabb maxElem (A.push elem ar))
-           | otherwise                   -> Leaf aabb maxElem (A.push elem ar)
-=======
         if | (A.length ar) >= maxElem -> subdivide (Leaf aabb maxElem (A.push elem ar))
            | otherwise                -> Leaf aabb maxElem (A.push elem ar)
 
@@ -81,63 +78,63 @@ insert elem qt = case qt of
 forEach : A.Array a -> b -> (a -> b -> b) -> b
 forEach a b f =
     let
-        isDone arr = if A.get arr 0 == Nothing then True else False
-        foo pred f {ar,acc} = case pred acc of
+        isDone arr = case A.get 0 arr of
+            Nothing -> True
+            _       -> False
+        foo pred f ar acc = case pred ar of
             True  -> Done (acc)
-            False -> Continue (foo pred f {ar = A.slice 1 (A.length ar), acc = f (A.get 0 ar) acc})
+            False -> Continue (\() -> foo pred f (A.slice 1 (A.length ar) ar) (f (fromJust (A.get 0 ar)) acc))
     in
-        trampoline <| foo isDone f {a,b}
+        trampoline <| foo isDone f a b
 
 -- Potentially won't work
-insertMany : A.Array Entity a -> Quadtree Entity a -> Quadtree Entity a
+insertMany : A.Array (Positioned a) -> Quadtree (Positioned a) -> Quadtree (Positioned a)
 insertMany a qt =
     forEach a qt insert
 
 removeA : a -> A.Array a -> A.Array a
 removeA a ar = A.filter (\i -> i /= a) ar
 
-remove : Entity a -> Quadtree Entity a -> Quadtree Entity a
+remove : Positioned a -> Quadtree (Positioned a) -> Quadtree (Positioned a)
 remove a qt = case qt of
     Node aabb nw ne se sw ->
-        if  | containsPointQT nw {a.x,a.y} -> remove a nw
-            | containsPointQT ne {a.x,a.y} -> remove a ne
-            | containsPointQT se {a.x,a.y} -> remove a se
+        if  | containsPointQT nw {x = a.x, y = a.y} -> remove a nw
+            | containsPointQT ne {x = a.x, y = a.y} -> remove a ne
+            | containsPointQT se {x = a.x, y = a.y} -> remove a se
             | otherwise                    -> remove a sw
     Leaf aabb maxElem ar -> Leaf aabb maxElem (removeA a ar)
 
-queryPoint: {x:Float, y:Float} -> Quadtree Entity a -> A.Array Entity a
+queryPoint: {x:Float, y:Float} -> Quadtree (Positioned a) -> A.Array (Positioned a)
 queryPoint ({x,y} as p) qt = case qt of
     Node aabb nw ne se sw ->
-        if  | containsPointQT nw {a.x,a.y} -> queryPoint p nw
-            | containsPointQT ne {a.x,a.y} -> queryPoint p ne
-            | containsPointQT se {a.x,a.y} -> queryPoint p se
+        if  | containsPointQT nw {x = p.x, y = p.y} -> queryPoint p nw
+            | containsPointQT ne {x = p.x, y = p.y} -> queryPoint p ne
+            | containsPointQT se {x = p.x, y = p.y} -> queryPoint p se
             | otherwise                    -> queryPoint p sw
     Leaf aabb maxElem ar -> ar
 
->>>>>>> 645e561... more qt functionality and removed unusedvectormath
-
 containsPoint : AABB -> { x:Float, y:Float } -> Bool
-containsPoint {{cx,cy},{w,h}} {x,y} =
-    if  x < cx + w / 2 and
-        x > cx - w / 2 and
-        y < cy + h / 2 and
-        y > cy - h / 2
+containsPoint {center,dimensions} {x,y} =
+    if  (x < center.x + dimensions.width / 2) &&
+        (x > center.x - dimensions.width / 2) &&
+        (y < center.y + dimensions.height / 2) &&
+        (y > center.y - dimensions.height / 2)
     then True else False
 
-containsPointQT : Quadtree a -> { x:Float, y:Float} -> Bool
+containsPointQT : Quadtree (Positioned a) -> { x:Float, y:Float} -> Bool
 containsPointQT qt xy =
     if containsPoint (aabbQT qt) xy then True else False
 
 intersectsAABB : AABB -> AABB -> Bool
-intersectsAABB {c1,d1} {c2,d2} =
-    isCollision c1 d1 c2 d2
+intersectsAABB a b =
+    isCollision a.center a.dimensions b.center b.dimensions
 
-allItems : Quadtree a -> A.Array a
+allItems : Quadtree (Positioned a) -> A.Array (Positioned a)
 allItems qt = case qt of
     Node aabb nw ne se sw -> A.append (allItems nw) <| A.append (allItems ne) <| A.append (allItems se) (allItems sw)
     Leaf aabb maxElem ar -> ar
 
-fixQT : Quadtree a -> Quadtree a
+fixQT : Quadtree (Positioned a) -> Quadtree (Positioned a)
 fixQT qt =
     insertMany (allItems qt)
     <| emptyQT (aabbQT qt) (maxElemQT qt)
--- src/elm/Game/Util.elm
+++ src/elm/Game/Util.elm
@@ -3,24 +3,3 @@ module Game.Util where
 lerp : Float -> Float -> Float -> Float -> Float -> Float
 lerp y0 y1 x0 x x1 =
     y0 + (y1 - y0) * ((x - x0) / (x1 - x0))
-
-type alias Vec2 = { x:Float, y:Float }
-
-dotProd : Vec2 -> Vec2 -> Float
-dotProd {x1,y1} {x2,y2} =
-    (x1 * x2) + (y1 * y2)
-
-scaleVec : Float -> Vec2 -> Vec2
-scaleVec s {x,y} = { x = s * x, y = s * y }
-
-scaleVecX : Float -> Vec2 -> Vec2
-scaleVecX s {x,y} = { x = s*x, y = y }
-
-scaleVecY : Float -> Vec2 -> Vec2
-scaleVecY s {x,y} = { x = x, y = y*s }
-
-addVec : Vec2 -> Vec2 -> Vec2
-addVec {x1,y1} {x2,y2} = { x = x1+x2, y = y1+y2 }
-
-subVec : Vec2 -> Vec2 -> Vec2
-subVec {x1,y1} {x2,y2} = { x = x1-x2, y = y1-y2 }
